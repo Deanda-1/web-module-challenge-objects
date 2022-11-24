2f29a400ea32438da6187d64f12e354d6116da86
Deandanoble1 <jimmynoble604@gmail.com>
2022-11-24 10:10:28 -0500

    task 4 complete

--- index.js
+++ index.js
@@ -88,11 +88,11 @@ Reyna's feedback is missing! Use what you know to do the following: (no function
   1. Add this feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
   2. log the reviews array to the console to check your work
 */
-
-
+reviews[7].feedback = 'this place is chill with really cool people, great for getting work done on weekdays'; 
+console.log(reviews);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
-Write a function that creates an object with name, rating, feedback, add the new review to the end of an array and returns the resulting array. 
+Write a funcion that creates an object with name, rating, feedback, add the new review to the end of an array and returns the resulting array. 
 
 Use the addReview function below to do the following:
   1. Receive an array in the FIRST parameter (invoke with the reviews array above)
